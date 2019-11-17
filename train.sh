9a6eb11b73dcbd21015ac18bea7d896cf532bce8
Ahsaas Bajaj <abajaj@gypsum.cm.cluster>
2019-11-16 22:31:00 -0500

    shell file

---
+++ train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=halfhalf
+#SBATCH --output=logs/hh_%j.txt  # output file
+#SBATCH -e logs/hh_%j.err        # File to which STDERR will be written
+#SBATCH --partition=titanx-long # Partition to submit to 
+#SBATCH --gres=gpu:1
+#
+#SBATCH --ntasks=1
+
+python -u train_model.py ./Data/train_ann_encoded.csv ./Data/val_ann_encoded.csv ../../Data 100
+#sleep 1
+exit
+
