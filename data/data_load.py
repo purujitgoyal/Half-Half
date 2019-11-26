a70967e84d10df4bd74c97f92365973eb854c5af
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-26 13:32:17 -0500

    data load for val

--- data/data_load.py
+++ data/data_load.py
@@ -21,20 +21,38 @@ def _data_transformation():
     return transformation
 
 
-def get_data_loaders(train_csv, val_csv, data_dir, batch_size=32, num_workers=0):
+def get_data_loaders(val_csv, data_dir, batch_size=32, num_workers=0, train_csv=None):
     transformation = _data_transformation()
-    train_dataset = HalfHalfLabelsDataset(csv_file=train_csv, root_dir=os.path.join(data_dir, 'train', 'images'),
+
+    if train_csv is not None:
+        # Training phase
+        train_dataset = HalfHalfLabelsDataset(csv_file=train_csv, root_dir=os.path.join(data_dir, 'train', 'images'),
                                           transform=transformation, num_classes=79)
-    val_dataset = HalfHalfLabelsDataset(csv_file=val_csv, root_dir=os.path.join(data_dir, 'val', 'images'),
-                                        transform=transformation, num_classes=79)
-    image_datasets = {'train': train_dataset, 'val': val_dataset}
+        val_dataset = HalfHalfLabelsDataset(csv_file=val_csv, root_dir=os.path.join(data_dir, 'val', 'images'),
+                                            transform=transformation, num_classes=79)
+        image_datasets = {'train': train_dataset, 'val': val_dataset}
 
-    # print(image_datasets['train'][100])
-    dataloaders = {x: DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=num_workers)
-                   for x in ['train', 'val']}
-    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+        # print(image_datasets['train'][100])
+        dataloaders = {x: DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=num_workers)
+                       for x in ['train', 'val']}
+        dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 
-    return dataloaders, dataset_sizes
+        return dataloaders, dataset_sizes
+
+    else:
+        # Validation phase
+        val_dataset = HalfHalfLabelsDataset(csv_file=val_csv, root_dir=os.path.join(data_dir, 'val', 'images'),
+                                            transform=transformation, num_classes=79)
+        image_datasets = {'val': val_dataset}
+
+        dataloaders = {x: DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=num_workers)
+                       for x in ['val']}
+        dataset_sizes = {x: len(image_datasets[x]) for x in ['val']}
+
+        return dataloaders, dataset_sizes
+
+
+
 
 
 def imshow(inp, title=None):
