0094031e2a4fc85dba02cb561ba246ee840e2da3
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-12-01 13:40:53 -0500

    [HalfHalf] Shell for training

---
+++ train_halfhalf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=glorehalf
+#SBATCH --output=logshalf/hh_%j.txt  # output file
+#SBATCH -e logshalf/hh_%j.err        # File to which STDERR will be written
+#SBATCH --partition=2080ti-long # Partition to submit to
+#SBATCH --gres=gpu:1
+#SBATCH --mem=80G
+#
+#SBATCH --ntasks=1
+
+python -u train_model.py ./Data/train_ann_encoded.csv ./Data/val_ann_encoded.csv ../../Data 100 ./model/model_glore_baseline
+#sleep 1
+exit
+
