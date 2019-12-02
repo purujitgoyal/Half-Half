d0cc83f1a3a2ab1e5f6defec076fc020e4afb310
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-12-02 18:08:45 -0500

    GCN on halfhalf (param tuned)

--- train_halfhalf.sh
+++ train_halfhalf.sh
@@ -3,13 +3,13 @@
 #SBATCH --job-name=gcnhalf
 #SBATCH --output=logsgcnhalf/hh_%j.txt  # output file
 #SBATCH -e logsgcnhalf/hh_%j.err        # File to which STDERR will be written
-#SBATCH --partition=2080ti-long # Partition to submit to
+#SBATCH --partition=titanx-long # Partition to submit to
 #SBATCH --gres=gpu:1
 #SBATCH --mem=80G
 #
 #SBATCH --ntasks=1
 
-python -u train_model.py ./Data/train_ann_encoded.csv ./Data/val_ann_encoded.csv ../../Data 100 ./model/model_gcn_baseline ./Data/baseline_glove_word2vec.pkl ./Data/baseline_left_labels.pkl
+python -u train_model.py ./Data/train_ann_encoded.csv ./Data/val_ann_encoded.csv ../../Data 100 ./model/model_gcn_baseline_tuned ./Data/baseline_glove_word2vec.pkl ./Data/baseline_left_labels.pkl
 #sleep 1
 exit
 
